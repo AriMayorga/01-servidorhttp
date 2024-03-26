f349362ba2f1cc2d0b7b5e8056ff61a39735d73e
AriMayorga <arimayorga.tech@gmail.com>
2024-03-26 11:06:25 -0300

    mi primer commit

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+var num1 = 10;
+var num2 = 10;
+if (num1 == num2) {
+  alert("son iguales");
+} else {
+  alert("no son iguales");
+}
+
+var password = "1234";
+console.log("se cargo corectamente el archivo JS");
